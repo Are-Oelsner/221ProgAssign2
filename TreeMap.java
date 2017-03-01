69dd49bc7a54273c1962c6984391eceeb162c57d
areo279 <are.oelsner@richmond.edu>
2017-03-01 13:18:57 -0500

    Worked on implementing Binary Search Tree

--- TreeMap.java
+++ TreeMap.java
@@ -10,6 +10,153 @@ import java.util.NoSuchElementException;
  */
 public class TreeMap<K,V> extends AbstractSortedMap<K,V> {
 
+  private class BinaryTree<K,V> {
+    private class Node<K,V> {
+      //Private Variables for Node class
+      private Node parent;
+      private Node left;
+      private Node right;
+      private K k;
+      private V v;
+
+      //Constructors for Node class
+      public Node<K,V>() {
+        parent = null;
+        left = null;
+        right = null;
+        k = null;
+        v = null;
+      }
+
+      //Node constructor from another node
+      public Node<K,V>(Node node) {
+        this.parent = node.parent;
+        this.left = node.left;
+        this.right = node.right;
+        this.k = node.k;
+        this.v = node.v;
+      }
+
+      //Basic Node constructor
+      public Node<K,V>(K _k, V _v) {
+        parent = null;
+        k = _k;
+        v = _v;
+        left = null;
+        right = null;
+      }
+
+      //Complex Node constructor
+      public Node<K,V>(Node _parent, Node _left, Node _right, K _k, V _v) {
+        parent = _parent;
+        left = _left;
+        right = _right;
+        k = _k;
+        v = _v;
+      }
+
+      //Accessor Methods
+      public Node getParent() {return parent;}
+      public Node getLeft() {return left;}
+      public Node getRight() {return right;}
+
+      public K getK() {return k;}
+      public V getV() {return v;}
+
+      //Setter Methods
+      public void setParent(Node _parent) {parent = _parent;}
+      public void setLeft(Node _left) {left = _left;}
+      public void setRight(Node _right) {right = _right;}
+
+      public void setK(K _k) {k = _k;}
+      public void setV(V _v) {v = _v;}
+    }
+
+
+    //BinaryTree class
+    //Private Variables
+    private Node<K,V> root;
+    private int size;
+
+    //Constructors for BinaryTree class
+    public BinaryTree/*generics*/() {
+      root = null;
+    }
+
+    //BinaryTree class methods
+    //Insert function: Inserts a key value pair into the binary search tree
+    public void insert(K k, V v) {//I wasn't sure how we would pass key-value pairs
+      Node<> ins = new Node<>(k,v); // As entries or as k and v separately
+      insertHelper(ins, root);
+    }
+
+    //Recursive helper function for Node insertion
+    private void insertHelper(Node ins, Node node) {
+      if(node == null) {
+        node = ins;
+        size++;
+      }
+      else if(k > root.getK()) {
+        insertHelper(ins, node.getRight());
+      }
+      else{
+        insertHelper(ins, node.getLeft());
+      }
+    }
+
+    //Find function: finds a node in the tree with value n
+    public Node<K,V> find(K k) {
+      return findHelper(k, root);
+
+    //Recursive helper function for finding Nodes
+    private Node<> findHelper(K k, Node node) {//iffy generics
+      if(node.getK() == k) {//Need to use comparator?
+        return node;
+      }
+      else if(k > root.getK()) {
+        findHelper(k, node.getRight());
+      }
+      else{
+        findHelper(k, node.getLeft());
+      }
+    }
+
+    //Delete Function
+    public Node<> delete(K k) {
+      Node node = find(k);
+      Node left = node.getLeft();
+      Node right = node.getRight();
+      Node ans = new Node(node);
+      left.setParent(node.getParent());
+      if(node.isLeft()) {
+        node.getParent().setLeft(node.getLeft());
+      }
+      else{
+        node.getParent().setRight(node.getRight());
+      }
+      if(left.getRight() == null) {
+        left.setRight(right);
+      }
+      else{
+        Node temp = left.getRight();
+        while(temp.getRight() != null) {
+          temp = temp.getRight();
+        }
+        temp.setRight(right);
+      }
+      size--;
+    }
+
+
+
+  }
+/*Do we create a Binary Tree as a private variable?
+ *
+ *protected BalanceableBinaryTree<K,V> tree = new BalanceableBinaryTree<>();
+ *
+ *^^Textbook implementation on pg. 467
+ */
+
   /** Constructs an empty map using the natural ordering of keys. */
   public TreeMap() {
     super();                  // the AbstractSortedMap constructor
@@ -21,6 +168,7 @@ public class TreeMap<K,V> extends AbstractSortedMap<K,V> {
    */
   public TreeMap(Comparator<K> comp) {
     super(comp);              // the AbstractSortedMap constructor
+    //tree.addRoot(null); //Relies on having BinaryTree<K,V> tree
   }
 
   /**
@@ -29,6 +177,7 @@ public class TreeMap<K,V> extends AbstractSortedMap<K,V> {
    */
   @Override
   public int size() {
+    //return (tree.size() - 1) / 2; //Textbook
     return -1;
   }
 
@@ -40,6 +189,13 @@ public class TreeMap<K,V> extends AbstractSortedMap<K,V> {
    */
   @Override
   public V get(K key) throws IllegalArgumentException {
+    /*Textbook Implementation
+     * checkKey(key);
+     * Position<Entry<K,V>> p = treeSearch(root(), key);
+     * rebalanceAccess(p);
+     * if (isExternal(p)) return null;
+     * return p.getElement().getValue();
+     */
     return null;
   }
 
